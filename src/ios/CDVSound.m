4ad6a4bdcc0da973ed19f359c62376abf1eceff2
eve <eve@ironstar.pw>
2017-08-13 18:28:31 +0300

    aaa

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -246,7 +246,7 @@
             // Subscribe to the AVPlayerItem's PlaybackStalledNotification notification.
             [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(itemStalledPlaying:) name:AVPlayerItemPlaybackStalledNotification object:playerItem];
 
-            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(itemStalledPlaying:) name:AVPlayerItemNewErrorLogEntryNotification object:playerItem];
+           // [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(itemStalledPlaying:) name:AVPlayerItemNewErrorLogEntryNotification object:playerItem];
             [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(itemStalledPlaying:) name:AVPlayerItemFailedToPlayToEndTimeNotification object:playerItem];
             
             
